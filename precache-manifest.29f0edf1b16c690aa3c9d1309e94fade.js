ba7b7afc943ab1f0f2c818db31c3a7682d0c5872
prachikapadia <prachi.kapadia@hotmail.com>
2019-08-14 23:46:50 -0400

    Updates

---
+++ precache-manifest.29f0edf1b16c690aa3c9d1309e94fade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd83417afa751a22d09b343bddd25ed8",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "489b767b01e3bd46cc6e",
+    "url": "/todo-list/static/css/main.20583bb8.chunk.css"
+  },
+  {
+    "revision": "175b832157d8bdb5e514",
+    "url": "/todo-list/static/js/2.3dd3065b.chunk.js"
+  },
+  {
+    "revision": "489b767b01e3bd46cc6e",
+    "url": "/todo-list/static/js/main.b5d65ab8.chunk.js"
+  },
+  {
+    "revision": "51236a9f9133e160b8c3",
+    "url": "/todo-list/static/js/runtime~main.fb7e4658.js"
+  }
+]);
